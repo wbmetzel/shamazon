acb6693eca897114809f333d30bda24538f41c0f
wbmetzel <w.metzel@gmail.com>
2021-11-22 21:37:08 -0500

    partial checkout implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Checkout from './components/Checkout';
 import NotFound from './components/NotFound';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+
     <Router basename="/shamazon">
       <div className="app">
+        <Header />
         <Routes>
           <Route path="*" element={<NotFound />}/>
           <Route path="/checkout" element={
             <div>
-              <Header />
-              <h1>oh yay...</h1>
+              <Checkout />
             </div>
           } />
           <Route path="/" element={
             <div>
-              <Header />
               <Home />
             </div>
           } />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { Link } from "react-router-dom";
 
 function Header() {
   return(
     <div className="header">
+      <Link to="/">
       <img
         className="header_logo"
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo"/>
+      </Link>
 
       <div className="header_search">
         <input className="header_searchInput" type="text"/>
@@ -32,10 +35,12 @@ function Header() {
         </div>
       </div>
 
-      <div className="header_itemCart">
-        <ShoppingCartIcon />
-        <span className="header_item_2 header_cartCount">0</span>
-      </div>
+      <Link to="/checkout">
+        <div className="header_itemCart">
+          <ShoppingCartIcon />
+          <span className="header_item_2 header_cartCount">0</span>
+        </div>
+      </Link>
     </div>
   );
 }
