63bc10150c990284868616ef907b7a12dd25aab8
wbmetzel <w.metzel@gmail.com>
2021-11-30 04:50:43 -0500

    login started, firebase initialized and API running

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { createUser, loginUser } from "../firebase";
 
-function login() {
+function Login() {
+  //const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = e => {
+    e.preventDefault();
+    loginUser(email, password).then((response) => {
+      // TODO: HANDLE ERRORS (separate by response code), redirect to home, update user creds to site
+      console.log(response);
+    });
+
+  }
+
+  const register = e => {
+    e.preventDefault();
+    createUser(email, password).then((user) => {
+      console.log(user);
+    }).then((error) => {
+      console.log(error.message);
+    });
+
+
+  }
+
   return (
     <div className="login">
       <Link to="/">
@@ -11,23 +36,24 @@ function login() {
 
       <div className="login_container">
         <h1>Sign-In</h1>
+
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 
-          <button className="login_signinButton">Sign In</button>
+          <button type="submit" onClick={signIn} className="login_signinButton">Sign In</button>
         </form>
 
         <p>By continuing, you agree to Shamazon's Conditions of Use and Privacy Notice.</p>
 
-        <button className="login_registerButton">Create Shamazon account</button>
+        <button onClick={register} className="login_registerButton">Create Shamazon account</button>
       </div>
 
     </div>
   )
 }
 
-export default login
+export default Login
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,55 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtp9Sz64XDVJmJdzOzs9Pmm673syDXTKs",
-  authDomain: "sh-56de6.firebaseapp.com",
-  projectId: "sh-56de6",
-  storageBucket: "sh-56de6.appspot.com",
-  messagingSenderId: "766326194982",
-  appId: "1:766326194982:web:d2697f4993a8d22850b7e7",
-  measurementId: "G-Q75WL9VQD7"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize database
+const db = getFirestore(app);
+
+// Authentication handler
+const auth = getAuth();
+
+
+/*
+* Register new user
+*/
+const createUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      return userCredential.user;
+    })
+    .catch((error) => {
+      // Unless there's a need, just send what's most important
+      return error.message;
+    });
+};
+
+/*
+* Login existing user account
+*/
+const loginUser = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      return userCredential.user;
+      // TODO: send response status here and in catch (200, 400, etc)
+      //const user = userCredential.user;
+      //return new Response({"status": 200 ,"body": user});
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+export {createUser,loginUser};
